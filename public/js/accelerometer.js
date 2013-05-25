1e01621197b2ad808017fc572973c5bb35f1153f
Jesper Petersson <jesper@jpetersson.se>
2013-05-25 11:25:35 +0200

    Added the accelerometer js

---
+++ public/js/accelerometer.js
@@ -0,0 +1,75 @@
+var app = {};
+
+(function(app) {
+
+	app.Accelerometer = function() {
+
+		var leftCallback, rightCallback, neutralCallback;
+		var x = 0;
+		var lastX = 0;
+	
+		this.init = function() {
+			window.ondevicemotion = function(event) {  
+				onMotion(event);
+			}
+		};
+
+		this.onLeft = function(callback) {
+			leftCallback = callback;
+		};
+
+		this.onRight = function(callback) {
+			rightCallback = callback;
+		};
+
+		this.onNeutral = function(callback) {
+			neutralCallback = callback;
+		};
+
+		var onMotion = function(event) {
+			x = event.accelerationIncludingGravity.x;  
+			y = event.accelerationIncludingGravity.y;  
+			z = event.accelerationIncludingGravity.z;  
+
+			if(typeof(leftCallback) === 'function' && analyzeShakeLeft(x, lastX)) {
+				leftCallback();
+			}
+			if(typeof(rightCallback) === 'function' && analyzeShakeRight(x, lastX)) {
+				rightCallback();
+			}
+			if(typeof(neutralCallback) === 'function' && analyzeShakeNeutral(x, lastX)) {
+				neutralCallback();
+			}
+			lastX = x;
+		};
+
+		var analyzeShakeLeft = function(x, lastX) {
+			return (x < lastX) && x < -5;
+		};
+
+		var analyzeShakeRight = function(x, lastX) {
+			return (x > lastX) && x > 5;
+		};
+
+		var analyzeShakeNeutral = function(x, lastX) {
+			return Math.abs(x) <= 2;
+		};
+
+	};
+
+})(app);
+
+var acc = new app.Accelerometer();
+acc.init();
+
+acc.onLeft(function() {
+	document.getElementsByTagName('p')[0].innerHTML = "left!<br />";
+});
+
+acc.onRight(function() {
+	document.getElementsByTagName('p')[0].innerHTML = "right!<br />";
+});
+
+acc.onNeutral(function() {
+	document.getElementsByTagName('p')[0].innerHTML = "neutral!<br />";
+});
