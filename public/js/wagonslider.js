7fab5e1be2b5cd97da71158b79b839bdd6e7bf39
par.dahlman <par.dahlman@valtech.se>
2013-05-25 17:45:17 +0200

    removed input argument and added support for a mobile curve

--- public/js/wagonslider.js
+++ public/js/wagonslider.js
@@ -2,15 +2,25 @@ var app = app || {};
 
 (function (app) {
 	var width = 
-	app.initializeSlider = function (points) {
-
+	app.initializeSlider = function () {
+		var desktopPoints = [25, 250, 450, 0, -455, 0, 700, 50];
+		var mobilePoints = [25, 350, 25, -175, 50, 175, 425, 50];
 		var windowWidth = window.innerWidth;
+
+
 		var topImgWidth = 1312;
 		var zoomRatio = windowWidth / topImgWidth;
 		if (zoomRatio > 1) {
 			zoomRatio = 1;
 		}
 
+		var points;
+		if (windowWidth > 700) {
+			points = desktopPoints;
+		}
+		else {
+			points = mobilePoints;
+		}
 		var scaledPoints = $.map(points, function (point) { return point * zoomRatio })
 		var pathslider = $('#p_curve');
 		pathslider.pathslider({
@@ -44,7 +54,6 @@ var app = app || {};
 
 		// Add events
 		pathslider.bind('create.pathslider update.pathslider start.pathslider slide.pathslider change.pathslider stop.pathslider', function (e, s) {
-			//$('body').append('<li>' + e.type + ' : ' + s.percent + '</li>');
 			imageUpdateDependingOnSlider(s.percent);
 		});
 	};
@@ -57,24 +66,24 @@ var app = app || {};
 
 
 	var imageUpdateDependingOnSlider = function (sliderValue) {
-		if (0 < sliderValue && sliderValue <= 25) {
+		if (20 < sliderValue && sliderValue <= 25) {
 			//disaster
-			updateZambiaAndIsland('img/zambia-1.png', 'img/island-1.png');
+			updateZambiaAndIsland('img/zambia-1.png', 'img/island-4.png');
 		}
 
 		if (25 < sliderValue && sliderValue <= 50) {
 			//slum
-			updateZambiaAndIsland('img/zambia-2.png', 'img/island-2.png');
+			updateZambiaAndIsland('img/zambia-2.png', 'img/island-3.png');
 		}
 
 		if (50 < sliderValue && sliderValue <= 75) {
 			//decent
-			updateZambiaAndIsland('img/zambia-3.png', 'img/island-3.png');
+			updateZambiaAndIsland('img/zambia-3.png', 'img/island-2.png');
 		}
 
-		if (75 < sliderValue && sliderValue <= 100) {
+		if (75 < sliderValue && sliderValue <= 80) {
 			//awsome
-			updateZambiaAndIsland('img/zambia-4.png', 'img/island-4.png');
+			updateZambiaAndIsland('img/zambia-4.png', 'img/island-1.png');
 		}
 
 
