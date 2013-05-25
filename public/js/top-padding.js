87fc1da97c72b3a61998fca2a7754e4d3d3bd695
Hannes Johansson <hannes.johansson@gmail.com>
2013-05-25 11:43:26 +0200

    Added padding top script yo

---
+++ public/js/top-padding.js
@@ -0,0 +1,43 @@
+// ------------------------------------------------
+// Function to add top padding on resize
+// ------------------------------------------------
+
+// Separate function object
+var topPaddingFromLogoMethods = (function () {
+    var selectors = {
+        image: '.top-img'
+    };
+    function init ($element) {
+        addPadding($element);
+        $(window).resize(function() {
+            addPadding($element);
+        });
+    }
+    function addPadding($element){
+        console.log('adding padding');
+        var imageHeight = $(selectors.image).height(),
+        paddingTop = imageHeight / 1.5;
+        $element.css('padding-top',paddingTop + 'px');
+    }
+
+    var base = {
+        selectors : selectors,
+        init: init,
+        addPadding: addPadding
+    };
+    return base;
+})();
+
+// Jquery plugin
+(function( $ ){
+    $.fn.topPaddingFromLogo = function() {
+        return this.each(function() {
+            var base = topPaddingFromLogoMethods;
+            var $self = $(this);
+            // Add padding
+            base.init($self);
+        });
+    };
+})( jQuery );
+
+$('.inner').topPaddingFromLogo();
